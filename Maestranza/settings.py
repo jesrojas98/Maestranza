135a84479ba31e0050f9d933a5f8d66cfd1e45e1
jesrojas98 <jes.rojasg@duocuc.cl>
2025-06-13 17:40:58 -0400

    Solucion whitenoise

--- Maestranza/settings.py
+++ Maestranza/settings.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = [
 ]
 
 # ✅ WHITENOISE CONFIGURACIÓN
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
