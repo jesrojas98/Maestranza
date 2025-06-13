7063ea5dcdd5f26afac170cba97bcde3ffd7ea17
jesrojas98 <jes.rojasg@duocuc.cl>
2025-06-13 19:23:44 -0400

    correccion de login intento 3

--- Maestranza/urls.py
+++ Maestranza/urls.py
@@ -22,22 +22,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('inventario.urls')),
-    
+
     path('admin/', admin.site.urls),
     
-    
-    # Autenticación
-    path('login/', auth_views.LoginView.as_view(
-        template_name='registration/login.html',
-        redirect_authenticated_user=True
-    ), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password-change/', auth_views.PasswordChangeView.as_view(
-        template_name='registration/password_change.html'
-    ), name='password_change'),
-    path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(
-        template_name='registration/password_change_done.html'
-    ), name='password_change_done'),
+
 ]
 
 # Servir archivos estáticos y media en desarrollo
