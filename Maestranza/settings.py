4ee85f01e99502d106eda8d32a8697b569f999ca
jesrojas98 <jes.rojasg@duocuc.cl>
2025-06-13 17:33:25 -0400

    Add PyMySQL for MySQL connection

--- Maestranza/settings.py
+++ Maestranza/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import pymysql
 from decouple import config
 
+pymysql.install_as_MySQLdb()
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
